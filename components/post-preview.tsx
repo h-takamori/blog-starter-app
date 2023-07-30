9dc5fb089aa7301d27ff9179d91e5ed0d1a52378
Hiromu Takamori <htakdev@gmail.com>
2023-07-30 19:29:43 +0900

    feat: トップページの投稿一覧をグリッド丸ごとクリック可能に変更し、ホバーエフェクトを追加

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -22,24 +22,23 @@ const PostPreview = ({
   slug,
 }: Props) => {
   return (
-    <div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
-          className="hover:underline"
-        >
-          {title}
-        </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
-      </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+    <div className="hover:opacity-60 hover:duration-300">
+      <Link
+        as={`/posts/${slug}`}
+        href="/posts/[slug]"
+      >
+        <div className="mb-5">
+          <CoverImage title={title} src={coverImage} />
+        </div>
+        <h3 className="text-3xl mb-3 leading-snug">
+            {title}
+        </h3>
+        <div className="text-lg mb-4">
+          <DateFormatter dateString={date} />
+        </div>
+        <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+        <Avatar name={author.name} picture={author.picture} />
+      </Link>
     </div>
   )
 }
