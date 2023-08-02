30c02bbd06ef877a336a75f91e5fb0f92de887f5
Hiromu Takamori <htakdev@gmail.com>
2023-08-02 14:18:34 +0900

    update: 記事の投稿ページのデザイン修正

--- pages/blog-form.tsx
+++ pages/blog-form.tsx
@@ -203,7 +203,7 @@ export default function BlogForm() {
         <button
           type="submit"
           disabled={hasError}
-          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
         >
           送信
         </button>
