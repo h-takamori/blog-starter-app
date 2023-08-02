85206c045ff4589d2e94ae57662d9e6873434885
Hiromu Takamori <htakdev@gmail.com>
2023-08-02 13:27:07 +0900

    fix: エラーへの対処

--- pages/blog-form.tsx
+++ pages/blog-form.tsx
@@ -61,7 +61,7 @@ export default function BlogForm() {
 
   // React Hook Formとyupを使ってフォームを管理する
   const { register, handleSubmit, formState } = useForm<FormValues>({
-    resolver: yupResolver(schema),
+    resolver: yupResolver(schema) as any,
     defaultValues: {
       slug: "",
       title: "",
@@ -191,7 +191,7 @@ export default function BlogForm() {
           <label htmlFor="content" className="block text-sm font-medium leading-6 text-gray-900">本文</label>
           {/* <input id="content" type="text" {...register("content")} /> */}
           <div className="flex rounded-md ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <textarea id="content" rows="10" cols="50" {...register("content")}
+            <textarea id="content" rows={10} cols={50} {...register("content")}
               className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
               />
             {formState.errors.content && (
