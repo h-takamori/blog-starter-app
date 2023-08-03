def0967affbedfdee87f304adeaf842bd9964998
Hiromu Takamori <htakdev@gmail.com>
2023-08-03 19:47:04 +0900

    feat: Prismaの利用の準備

--- pages/blog-form.tsx
+++ pages/blog-form.tsx
@@ -38,7 +38,7 @@ export default function BlogForm() {
   // Next.jsのAPIに送信する関数
   const submitForm = async (data: FormValues) => {
     try {
-      const response = await fetch("/api/blog", {
+      const response = await fetch("/api/create-post", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
