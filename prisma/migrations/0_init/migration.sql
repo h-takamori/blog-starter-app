e0e5e0f225f654cc71e93ab2567952a347a0aa4a
Hiromu Takamori <htakdev@gmail.com>
2023-08-03 16:11:56 +0900

    feat: Prismaの利用の準備

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,27 +1,27 @@
 -- CreateTable
-CREATE TABLE "Blog_User" (
+CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(255),
     "picture" VARCHAR(255),
 
-    CONSTRAINT "Blog_User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Post" (
+CREATE TABLE "post" (
     "id" SERIAL NOT NULL,
     "slug" VARCHAR(255),
     "title" VARCHAR(255),
     "excerpt" VARCHAR(1023),
     "coverimage" VARCHAR(255),
     "date" VARCHAR(255),
-    "authorid" INTEGER,
+    "authorid" INTEGER NOT NULL,
     "content" TEXT,
     "ogimage" VARCHAR(255),
 
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "post_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorid_fkey" FOREIGN KEY ("authorid") REFERENCES "Blog_User"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "post" ADD CONSTRAINT "post_authorid_fkey" FOREIGN KEY ("authorid") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
