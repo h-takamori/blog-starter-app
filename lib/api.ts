c85ca4fc04d49ab67e5fc6e5069d9d1b3672dd2e
Hiromu Takamori <htakdev@gmail.com>
2023-08-04 02:06:36 +0900

    style: 不要なコメントやログ出力の削除

--- lib/api.ts
+++ lib/api.ts
@@ -1,51 +1,3 @@
-// import fs from 'fs'
-// import { join } from 'path'
-// import matter from 'gray-matter'
-
-// const postsDirectory = join(process.cwd(), '_posts')
-
-// export function getPostSlugs() {
-//   return fs.readdirSync(postsDirectory)
-// }
-
-// export function getPostBySlug(slug: string, fields: string[] = []) {
-//   const realSlug = slug.replace(/\.md$/, '')
-//   const fullPath = join(postsDirectory, `${realSlug}.md`)
-//   const fileContents = fs.readFileSync(fullPath, 'utf8')
-//   const { data, content } = matter(fileContents)
-
-//   type Items = {
-//     [key: string]: string
-//   }
-
-//   const items: Items = {}
-
-//   // Ensure only the minimal needed data is exposed
-//   fields.forEach((field) => {
-//     if (field === 'slug') {
-//       items[field] = realSlug
-//     }
-//     if (field === 'content') {
-//       items[field] = content
-//     }
-
-//     if (typeof data[field] !== 'undefined') {
-//       items[field] = data[field]
-//     }
-//   })
-
-//   return items
-// }
-
-// export function getAllPosts(fields: string[] = []) {
-//   const slugs = getPostSlugs()
-//   const posts = slugs
-//     .map((slug) => getPostBySlug(slug, fields))
-//     // sort posts by date in descending order
-//     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
-//   return posts
-// }
-
 import { PrismaClient } from '@prisma/client'
 
 export async function getAllPosts(fields: string[] = []) {
@@ -82,10 +34,6 @@ export async function getAllPosts(fields: string[] = []) {
       }
     })
 
-    // authorのデータも追加
-    // items['author'] = post.author
-    console.log(`items['author'] = ${items['author']}`)
-    
     return items
   })
 }
@@ -122,10 +70,6 @@ export async function getPostBySlug(slug: string, fields: string[] = []) {
       items[field] = post[field]
     }
   })
-
-  // // authorのデータも追加
-  // items['author'] = post.author.name
-  // items['authorpicture'] = post.author.picture
 
   return items
 }
