53451f4e3d78c4cde8b316682f2614e395695a47
Hiromu Takamori <htakdev@gmail.com>
2023-08-14 10:11:12 +0900

    style: サイドメニューに背景色を設定

--- components/side-menu.tsx
+++ components/side-menu.tsx
@@ -4,7 +4,7 @@ type Props = {
   
   const SideMenu = ({ children }: Props) => {
     return (
-      <div style={{position: "fixed", zIndex: 1}} className="top-2.5 right-2.5 px-2.5 py-2.5">
+      <div style={{position: "fixed", zIndex: 1}} className="top-2.5 right-2.5 px-2.5 py-2.5 bg-gray-500/10">
         {children}
       </div>
     );
